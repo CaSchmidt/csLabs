55cc38c1feb5cd0c98bf96f50c5dbd4cce0f97d5
Carsten Schmidt <CaSchmidt@users.noreply.github.com>
2017-02-26 12:00:16 +0100

    csQt: Support for Qt v5.8.0.

--- cslibs/csQt/src/csImageTip.cpp
+++ cslibs/csQt/src/csImageTip.cpp
@@ -37,7 +37,7 @@
 #include <QtGui/QPixmap>
 #include <QtCore/QPoint>
 #include <QtWidgets/QWidget>
-#include <QtWidgets/5.7.0/QtWidgets/private/qeffects_p.h>
+#include <QtWidgets/5.8.0/QtWidgets/private/qeffects_p.h>
 
 #include "csQt/csImageTip.h"
 
