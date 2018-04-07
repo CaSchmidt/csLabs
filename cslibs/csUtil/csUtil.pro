68e972a9411bdc155e7d373a4645cb72a52e01a9
Carsten Schmidt <CaSchmidt@users.noreply.github.com>
2018-04-07 17:28:55 +0200

    csUtil: Implemented code page convertersion.

--- cslibs/csUtil/csUtil.pro
+++ cslibs/csUtil/csUtil.pro
@@ -16,6 +16,11 @@ DLLDESTDIR = ../../bin
 
 DEFINES += CS_BUILD_UTIL_LIB
 
+# ICU
+DEFINES += U_COMMON_IMPLEMENTATION U_STATIC_IMPLEMENTATION
+
+
+# Dependencies ###############################################################
 
 INCLUDEPATH += ./include
 DEPENDPATH  += ./include
@@ -23,6 +28,12 @@ DEPENDPATH  += ./include
 INCLUDEPATH += ../include
 DEPENDPATH  += ../include
 
+INCLUDEPATH += ./3rdparty/icu-ucnv/src
+
+LIBS += advapi32.lib
+
+
+# Project ####################################################################
 
 SOURCES += \
     3rdparty/double-conversion/src/bignum-dtoa.cc \
@@ -33,10 +44,73 @@ SOURCES += \
     3rdparty/double-conversion/src/fast-dtoa.cc \
     3rdparty/double-conversion/src/fixed-dtoa.cc \
     3rdparty/double-conversion/src/strtod.cc \
+    3rdparty/icu-ucnv/src-dat/icudt61l_dat.c \
+    3rdparty/icu-ucnv/src/appendable.cpp \
+    3rdparty/icu-ucnv/src/charstr.cpp \
+    3rdparty/icu-ucnv/src/cmemory.cpp \
+    3rdparty/icu-ucnv/src/cstring.cpp \
+    3rdparty/icu-ucnv/src/locid.cpp \
+    3rdparty/icu-ucnv/src/locmap.cpp \
+    3rdparty/icu-ucnv/src/putil.cpp \
+    3rdparty/icu-ucnv/src/resource.cpp \
+    3rdparty/icu-ucnv/src/stringpiece.cpp \
+    3rdparty/icu-ucnv/src/uarrsort.cpp \
+    3rdparty/icu-ucnv/src/ucln_cmn.cpp \
+    3rdparty/icu-ucnv/src/ucmndata.cpp \
+    3rdparty/icu-ucnv/src/ucnv.cpp \
+    3rdparty/icu-ucnv/src/ucnv2022.cpp \
+    3rdparty/icu-ucnv/src/ucnv_bld.cpp \
+    3rdparty/icu-ucnv/src/ucnv_cb.cpp \
+    3rdparty/icu-ucnv/src/ucnv_cnv.cpp \
+    3rdparty/icu-ucnv/src/ucnv_ct.cpp \
+    3rdparty/icu-ucnv/src/ucnv_err.cpp \
+    3rdparty/icu-ucnv/src/ucnv_ext.cpp \
+    3rdparty/icu-ucnv/src/ucnv_io.cpp \
+    3rdparty/icu-ucnv/src/ucnv_lmb.cpp \
+    3rdparty/icu-ucnv/src/ucnv_u16.cpp \
+    3rdparty/icu-ucnv/src/ucnv_u32.cpp \
+    3rdparty/icu-ucnv/src/ucnv_u7.cpp \
+    3rdparty/icu-ucnv/src/ucnv_u8.cpp \
+    3rdparty/icu-ucnv/src/ucnvbocu.cpp \
+    3rdparty/icu-ucnv/src/ucnvhz.cpp \
+    3rdparty/icu-ucnv/src/ucnvisci.cpp \
+    3rdparty/icu-ucnv/src/ucnvlat1.cpp \
+    3rdparty/icu-ucnv/src/ucnvmbcs.cpp \
+    3rdparty/icu-ucnv/src/ucnvscsu.cpp \
+    3rdparty/icu-ucnv/src/ucol_swp.cpp \
+    3rdparty/icu-ucnv/src/udata.cpp \
+    3rdparty/icu-ucnv/src/udatamem.cpp \
+    3rdparty/icu-ucnv/src/udataswp.cpp \
+    3rdparty/icu-ucnv/src/uenum.cpp \
+    3rdparty/icu-ucnv/src/uhash.cpp \
+    3rdparty/icu-ucnv/src/uinit.cpp \
+    3rdparty/icu-ucnv/src/uinvchar.cpp \
+    3rdparty/icu-ucnv/src/uloc.cpp \
+    3rdparty/icu-ucnv/src/uloc_keytype.cpp \
+    3rdparty/icu-ucnv/src/uloc_tag.cpp \
+    3rdparty/icu-ucnv/src/umapfile.cpp \
+    3rdparty/icu-ucnv/src/umath.cpp \
+    3rdparty/icu-ucnv/src/umutex.cpp \
+    3rdparty/icu-ucnv/src/unistr.cpp \
+    3rdparty/icu-ucnv/src/uobject.cpp \
+    3rdparty/icu-ucnv/src/uresbund.cpp \
+    3rdparty/icu-ucnv/src/uresdata.cpp \
+    3rdparty/icu-ucnv/src/ustr_cnv.cpp \
+    3rdparty/icu-ucnv/src/ustrenum.cpp \
+    3rdparty/icu-ucnv/src/ustrfmt.cpp \
+    3rdparty/icu-ucnv/src/ustring.cpp \
+    3rdparty/icu-ucnv/src/ustrtrns.cpp \
+    3rdparty/icu-ucnv/src/utf_impl.cpp \
+    3rdparty/icu-ucnv/src/utrace.cpp \
+    3rdparty/icu-ucnv/src/utrie2.cpp \
+    3rdparty/icu-ucnv/src/utypes.cpp \
+    3rdparty/icu-ucnv/src/uvector.cpp \
+    3rdparty/icu-ucnv/src/wintz.cpp \
     src/csCharConv.cpp \
     src/csCharConv-fp.cpp \
     src/csFormat.cpp \
-    src/csLogger.cpp
+    src/csLogger.cpp \
+    src/csTextConverter.cpp
 
 HEADERS += \
     ../include/csUtil/csCharConv.h \
@@ -47,4 +121,5 @@ HEADERS += \
     ../include/csUtil/csFormat.h \
     ../include/csUtil/csILogger.h \
     ../include/csUtil/csLogger.h \
-    ../include/csUtil/csRawConverter.h
+    ../include/csUtil/csRawConverter.h \
+    ../include/csUtil/csTextConverter.h
---
+++ cslibs/include/csUtil/csTextConverter.h
@@ -0,0 +1,115 @@
+/****************************************************************************
+** Copyright (c) 2018, Carsten Schmidt. All rights reserved.
+**
+** Redistribution and use in source and binary forms, with or without
+** modification, are permitted provided that the following conditions
+** are met:
+**
+** 1. Redistributions of source code must retain the above copyright
+**    notice, this list of conditions and the following disclaimer.
+**
+** 2. Redistributions in binary form must reproduce the above copyright
+**    notice, this list of conditions and the following disclaimer in the
+**    documentation and/or other materials provided with the distribution.
+**
+** 3. Neither the name of the copyright holder nor the names of its
+**    contributors may be used to endorse or promote products derived from
+**    this software without specific prior written permission.
+**
+** THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+** "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+** LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+** A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+** HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+** SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+** LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+** DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+** THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+** (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+** OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*****************************************************************************/
+
+#ifndef __CSTEXTCONVERTER_H__
+#define __CSTEXTCONVERTER_H__
+
+#include <list>
+#include <memory>
+#include <string>
+
+#include <csUtil/csutil_config.h>
+
+class csTextConverterData;
+
+class CS_UTIL_EXPORT csTextConverter {
+public:
+  ~csTextConverter();
+
+  csTextConverter(csTextConverter&&) = default;
+  csTextConverter& operator=(csTextConverter&&) = default;
+
+  bool isNull() const;
+
+  void clear();
+
+  const char *name() const;
+
+  std::string fromUnicode(const char16_t *s, const int len = -1) const;
+
+  inline std::string fromUnicode(const std::u16string& s) const
+  {
+    return fromUnicode(s.data(), static_cast<int>(s.size()));
+  }
+
+  std::u16string toUnicode(const char *s, const int len = -1) const;
+
+  inline std::u16string toUnicode(const std::string& s) const
+  {
+    return toUnicode(s.data(), static_cast<int>(s.size()));
+  }
+
+  static csTextConverter create(const char *name);
+
+  static std::list<std::string> listAvailable();
+
+private:
+  csTextConverter(csTextConverterData *ptr = nullptr);
+
+  csTextConverter(const csTextConverter&) = delete;
+  csTextConverter& operator=(const csTextConverter&) = delete;
+
+  std::unique_ptr<csTextConverterData> d;
+};
+
+// 8bit -> UTF-16 ////////////////////////////////////////////////////////////
+
+CS_UTIL_EXPORT std::u16string csAsciiToUnicode(const char *s, const int len = -1);
+
+inline std::u16string csAsciiToUnicode(const std::string& s)
+{
+  return csAsciiToUnicode(s.data(), static_cast<int>(s.size()));
+}
+
+CS_UTIL_EXPORT std::u16string csUtf8ToUnicode(const char *s, const int len = -1);
+
+inline std::u16string csUtf8ToUnicode(const std::string& s)
+{
+  return csUtf8ToUnicode(s.data(), static_cast<int>(s.size()));
+}
+
+// UTF-16 -> 8bit ////////////////////////////////////////////////////////////
+
+CS_UTIL_EXPORT std::string csUnicodeToAscii(const char16_t *s, const int len = -1);
+
+inline std::string csUnicodeToAscii(const std::u16string& s)
+{
+  return csUnicodeToAscii(s.data(), static_cast<int>(s.size()));
+}
+
+CS_UTIL_EXPORT std::string csUnicodeToUtf8(const char16_t *s, const int len = -1);
+
+inline std::string csUnicodeToUtf8(const std::u16string& s)
+{
+  return csUnicodeToUtf8(s.data(), static_cast<int>(s.size()));
+}
+
+#endif // __CSTEXTCONVERTER_H__
